6767b486ce436d7fd9a5e88d4af01fa397564530
Ryan Tong <ryantong@seas.upenn.edu>
2023-05-01 16:55:10 -0400

    paths

--- mpc/scripts/generate_waypoints.py
+++ mpc/scripts/generate_waypoints.py
@@ -5,34 +5,72 @@ import bezier
 
 # corners are a set of 4 points, corner point, entry, control, exit
 
-control_corner1 = np.array([9.7, 0.])
-control_corner2 = np.array([9.7, 8.7])
-control_corner3 = np.array([-13.6, 8.7])
-control_corner4 = np.array([-13.6, 0.])
+# corner 1 is the corner closest to K lab
+# corners are a set of 3 points, corner point, entry, control, exit, 
 
-wall_corner1 = np.array([8.93, 0.69])
-wall_corner2 = np.array([8.89, 7.78])
-wall_corner3 = np.array([-12.73, 7.73])
-wall_corner4 = np.array([-12.89, 0.72])
+# Raceline points
+# corner1 = np.array([np.array([6.87883,1.30847]), 
+#                     np.array([6.1366,0.0228705]), 
+#                     np.array([7.7, .5]), 
+#                     np.array([8.00807,3.29504])]) # wider gap = x,-y
 
-corner_radius = 0.7
+# corner2 = np.array([np.array([6.43737,8.39063]), 
+#                     np.array([7.76042,6.79449]), 
+#                     np.array([7.25, 9.25]), 
+#                     np.array([5.35579,9.53412])]) # wider gap = x,y
 
-entry_corner1 = np.array([control_corner1[0]-corner_radius, control_corner1[1]])
-exit_corner1 = np.array([control_corner1[0], control_corner1[1]+corner_radius])
+# corner3 = np.array([np.array([-15.5477,6.88314]), 
+#                     np.array([-14.3637,7.96213]), 
+#                     np.array([-16.5, 7.7]), 
+#                     np.array([-16.2773,5.91601])]) # wider gap = -x,y 
+#
+# corner4 = np.array([np.array([-15.3005,0.0180681]), 
+#                     np.array([-16.4105,1.27514]), 
+#                     np.array([-16.35, -1.05]), 
+#                     np.array([-13.4578,-0.87522])]) # wider gap = -x,-y
 
-entry_corner2 = np.array([control_corner2[0], control_corner2[1]-corner_radius])
-exit_corner2 = np.array([control_corner2[0]-corner_radius, control_corner2[1]])
+# Outside line points
+# corner1 = np.array([np.array([6.87883,1.30847]), 
+#                     np.array([7.1,0.0228705]), 
+#                     np.array([8.6, .1]), 
+#                     np.array([8.00807,3.29504])]) # wider gap = x,-y
 
-entry_corner3 = np.array([control_corner3[0]+corner_radius, control_corner3[1]])
-exit_corner3 = np.array([control_corner3[0], control_corner3[1]-corner_radius])
+# corner2 = np.array([np.array([6.43737,8.39063]), 
+#                     np.array([7.6,8.5]), 
+#                     np.array([7.5, 9.63]), 
+#                     np.array([6.4,9.53412])]) # wider gap = x,y
 
-entry_corner4 = np.array([control_corner4[0], control_corner4[1]+corner_radius])
-exit_corner4 = np.array([control_corner4[0]+corner_radius, control_corner4[1]])
+# corner3 = np.array([np.array([-15.5477,6.88314]), 
+#                     np.array([-15.6,8]), 
+#                     np.array([-16.5, 7.7]), 
+#                     np.array([-16.2773,5.91601])]) # wider gap = -x,y 
+#
+# corner4 = np.array([np.array([-15.3005,0.0180681]), 
+#                     np.array([-16.4105,-.2]), 
+#                     np.array([-16.1, -1.15]), 
+#                     np.array([-15.,-1.])]) # wider gap = -x,-y
 
-corner1 = np.array([wall_corner1, entry_corner1, control_corner1, exit_corner1])
-corner2 = np.array([wall_corner2, entry_corner2, control_corner2, exit_corner2])
-corner3 = np.array([wall_corner3, entry_corner3, control_corner3, exit_corner3])
-corner4 = np.array([wall_corner4, entry_corner4, control_corner4, exit_corner4])
+# Inside line points
+corner1 = np.array([np.array([6.87883,1.30847]), 
+                    np.array([6.87,0.75]), 
+                    np.array([7.25, .97]), 
+                    np.array([7.3,1.4])]) # wider gap = x,-y
+
+corner2 = np.array([np.array([6.43737,8.39063]), 
+                    np.array([6.88,8.5]), 
+                    np.array([6.62, 9.01]), 
+                    np.array([6.0,8.9])]) # wider gap = x,y
+
+corner3 = np.array([np.array([-15.5477,6.88314]), 
+                    np.array([-15.61,7.3]), 
+                    np.array([-16, 7.1]), 
+                    np.array([-16., 6.1])]) # wider gap = -x,y 
+
+corner4 = np.array([np.array([-15.3005,0.0180681]), 
+                    np.array([-15.7,0.15]), 
+                    np.array([-15.4, -.4]), 
+                    np.array([-15.,-0.3])]) # wider gap = -x,-y
+
 corners = np.stack([corner1, corner2, corner3, corner4])
 
 straight1 = np.linspace(corner1[-1], corner2[1], int(np.linalg.norm(corner1[-1] - corner2[1]) * 10), endpoint=False) #corner1 to corner2
@@ -49,20 +87,29 @@ for corner in corners:
     interp_corners.append(res.T)
 
     min_dist = np.linalg.norm(res.T - corner[0], axis=1)
-    # print(f"Min dist from corner: {min_dist.min()}")
+    print(f"Min dist from corner: {min_dist.min()}")
+    
+    # figure, axes = plt.subplots()
+    # circle = plt.Circle((corner[0, 0], corner[0, 1]), .5, fill=False)
+    # axes.set_aspect(1)
+    # axes.add_artist(circle)
+    # plt.scatter(corner[1:, 0], corner[1:, 1])
+    # plt.plot(res[0], res[1])
+    # plt.scatter(corner[0, 0], corner[0, 1], c='red')
+    # plt.show()
 
 full_path = np.concatenate([interp_corners[0], straight1, interp_corners[1], straight2, interp_corners[2], straight3, interp_corners[3], straight4])
-slowdown_entry = int(1.2 * 10.) # how far before entering the corner to start slowing down
-speedup_exit = int(0 * 10.) # how far before exiting the corner to start speeding up
+slowdown_entry = int(1.75 * 10.) # how far before entering the corner to start slowing down
+speedup_exit = int(.4 * 10.) # how far before exiting the corner to start speeding up
 straight_speed = 6.
-slowdown_speed = 2.
-speed_lookup = np.concatenate([np.ones(len(interp_corners[0])) * slowdown_speed, np.linspace(slowdown_speed, straight_speed, speedup_exit), 
+slowdown_speed = 3.
+speed_lookup = np.concatenate([np.ones(len(interp_corners[0])) * slowdown_speed, np.ones(speedup_exit) * slowdown_speed, 
     np.ones(len(straight1[speedup_exit:len(straight1)-slowdown_entry])) * straight_speed, np.ones(len(straight1[len(straight1)-slowdown_entry:])) * slowdown_speed,
-    np.ones(len(interp_corners[1])) * slowdown_speed, np.linspace(slowdown_speed, straight_speed, speedup_exit),
+    np.ones(len(interp_corners[1])) * slowdown_speed, np.ones(speedup_exit) * slowdown_speed,
     np.ones(len(straight2[speedup_exit:len(straight2)-slowdown_entry])) * straight_speed, np.ones(len(straight2[len(straight2)-slowdown_entry:])) * slowdown_speed,
-    np.ones(len(interp_corners[2])) * slowdown_speed, np.linspace(slowdown_speed, straight_speed, speedup_exit),
+    np.ones(len(interp_corners[2])) * slowdown_speed, np.ones(speedup_exit) * slowdown_speed,
     np.ones(len(straight3[speedup_exit:len(straight3)-slowdown_entry])) * straight_speed, np.ones(len(straight3[len(straight3)-slowdown_entry:])) * slowdown_speed,
-    np.ones(len(interp_corners[3])) * slowdown_speed, np.linspace(slowdown_speed, straight_speed, speedup_exit),
+    np.ones(len(interp_corners[3])) * slowdown_speed, np.ones(speedup_exit) * slowdown_speed,
     np.ones(len(straight4[speedup_exit:len(straight4)-slowdown_entry])) * straight_speed, np.ones(len(straight4[len(straight4)-slowdown_entry:])) * slowdown_speed])
 assert len(full_path) == len(speed_lookup)
 
--- mpc/scripts/overtaking_node.py
+++ mpc/scripts/overtaking_node.py
@@ -5,51 +5,103 @@ from rclpy.node import Node
 
 import numpy as np
 from sensor_msgs.msg import LaserScan
-from ackermann_msgs.msg import AckermannDriveStamped
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+from generate_waypoints import get_waypoints, get_speed_lookup
+from scipy.spatial.transform import Rotation
+
+RACE_PATH = 0
+INSIDE_PATH = 1
+OUTSIDE_PATH = 2
 
 class OverTaking(Node):
-    """ 
-    Implement Wall Following on the car
-    """
     def __init__(self):
         super().__init__('overtaking_node')
 
-        lidarscan_topic = '/scan'
-        drive_topic = '/drive'
-
         # TODO: create subscribers and publishers
-        self.subscription = self.create_subscription(LaserScan, '/scan',self.scan_callback, 10)
-        self.publisher_ = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+        #self.laser_sub = self.create_subscription(LaserScan, '/scan',self.scan_callback, 10)
+        self.odom_sub = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
+        self.drive_pub = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+        self.viz_pub = self.create_publisher(Marker, '/pure_pursuit', 10)
 
-        # TODO: set PID gains
-        self.kp = 12.0
-        self.kd = .6
-        self.ki =0.00
-        self.L=2.0
+        # Pure pursuit params
+        self.L = 1.2
+        self.K = .45
+        self.curr_waypoint = 0 # waypoint index
 
-        # TODO: store history
-        self.integral = 0.0
-        self.prev_error = 0.0
-        self.error = 0.0
+        # Overtaking params
+        self.curr_path = RACE_PATH
+        self.overtake_dist = 1.5 # How close should the car infront be before we try overtaking
 
-        # TODO: store any necessary values you think you'll need
+        self.waypoints = [] # array containing all waypoint arrays
+        self.speeds = []
+        race = 'waypoints/path.npy'
+        inside = 'waypoints/path.npy'
+        outside = 'waypoints/path.npy'
+        self.load_waypoints(race, inside, outside)
 
-        self.angle_min = -2.35
-        self.angle_incr = 0.004352
+    def viz_point(self, pose, id, thick=False):
+        marker = Marker()
+        marker.header.frame_id = 'map'
+        marker.ns = 'point'
+        marker.id = id
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.position.x = pose[0]
+        marker.pose.position.y = pose[1]
+        marker.pose.position.z = 0.
+        if thick:
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+        else:
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            marker.color.a = 1.0
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+        self.viz_pub.publish(marker)
+    
+    def viz_path(self):
+        viz_waypoints = []
+        for waypoint in self.waypoints[self.curr_path]:
+            p = Point()
+            p.x = waypoint[0]
+            p.y = waypoint[1]
+            p.z = 0.
+            viz_waypoints.append(p)
+        marker = Marker()
+        marker.header.frame_id = 'map'
+        marker.ns = 'path'
+        marker.id = 0
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
+        marker.points = viz_waypoints
+        marker.scale.x = 0.1
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        self.viz_pub.publish(marker)
+
+    def load_waypoints(self, race, inside, outside):
+        self.waypoints.append(get_waypoints())
+        self.waypoints.append(np.load(inside))
+        self.waypoints.append(np.load(outside))
+
+        self.speeds.append(np.ones(len(self.waypoints[RACE_PATH])) * 3.)
+        self.speeds.append(np.ones(len(self.waypoints[INSIDE_PATH])) * 3.)
+        self.speeds.append(np.ones(len(self.waypoints[OUTSIDE_PATH])) * 3.)
 
     def get_range(self, range_data, angle):
-        """
-        Simple helper to return the corresponding range measurement at a given angle. Make sure you take care of NaNs and infs.
-
-        Args:
-            range_data: single range array from the LiDAR
-            angle: between angle_min and angle_max of the LiDAR
-        #TODO: implement
-        Returns:
-            range: range measurement in meters at the given angle
-
-        """
-
         # The line below is: angle_in_lidar = 135deg - angle
         #angle_in_lidar = -1*self.angle_min - angle
         angle_in_lidar = -1*self.angle_min + angle
@@ -57,20 +109,17 @@ class OverTaking(Node):
         return range_data[index]
     
     def overtaking_check(self, msg, location, path, waypoint):
-        rangetocheck=1
+        rangetocheck = 1
         if (self.get_dist(waypoint,location)< rangetocheck):
-            pathgood=self.close_to_obstacle(waypoint,location,msg)
-            if (pathgood== False):
-                path= self.select_new_path(path)
+            pathgood = self.close_to_obstacle(waypoint,location,msg)
+            if (pathgood == False):
+                path = self.select_new_path(path)
     
- 
-                
-
     def get_dist(self, waypoint_location,car_location_in_world):
         return (((car_location_in_world[0]-waypoint_location[0])**2 + (car_location_in_world[1]-waypoint_location[1])**2 )**.5)
 
     def Select_new_path(path):
-        if(path==2):
+        if (path == 2):
             return 1
         else:
             return 2
@@ -93,43 +142,103 @@ class OverTaking(Node):
                     return False
         return True
 
-
-
-
     def scan_callback(self, msg):
+        pass
+
+    def find_waypoint(self, pose_in):
+        """ Find the next desired waypoint
+        Points are in 2D
+        1. Only look 'forward' in the waypoint array from the current (loops back at the end)
+        2. Find distance from current pose to each waypoint
+        3. Find the waypoint that is closest to the lookahead distance
+        4. Interpolate a waypoint if needed
         """
-        Callback function for LaserScan messages. Calculate the error and publish the drive message in this function.
 
-        Args:
-            msg: Incoming LaserScan message
+        # how many waypoints to look ahead, should be calculated based on L and how dense our waypoints are
+        # Dont want to look 'backwards' into the array
+        look_ahead = 60 # TODO change based on L and waypoint density
+        pose = np.array([pose_in.x, pose_in.y]) # ignore the z
+        curr_path = self.curr_path
+        curr_waypoints = self.waypoints[curr_path]
+        curr_speeds = self.speeds[curr_path]
 
-        Returns:
-            None
-        """
-        #how far we want to be from wall
-        dist=1.0 #placeholder
-        #range data
-        rangedata=msg.ranges
-        # TODO: replace with error calculated by get_error()
-        error = self.get_error(rangedata, dist) 
-        # we probably actually shouldnt do this  next part according to writeup idk i found v inside the pid 
-        #see writeup for velocity based on steering angle
-        # velocity =  # TODO: calculate desired car velocity based on error
-        velocity=.6
-        self.L=.5
-        self.pid_control(error, velocity) # TODO: actuate the car with PID
+        dists = np.linalg.norm(curr_waypoints - pose, axis=1)
+        
+        if self.curr_waypoint + look_ahead > len(curr_waypoints): # wrap around
+            waypoints = np.concatenate([curr_waypoints[self.curr_waypoint:], curr_waypoints[:self.curr_waypoint + look_ahead - len(curr_waypoints)]])
+            truncated_dists = np.concatenate([dists[self.curr_waypoint:], dists[:self.curr_waypoint + look_ahead - len(curr_waypoints)]])
+        else:
+            waypoints = curr_waypoints[self.curr_waypoint:self.curr_waypoint + look_ahead]
+            truncated_dists = dists[self.curr_waypoint:self.curr_waypoint + look_ahead]
+        
+        # Find closests waypoint
+        end = None
+        for i in range(len(truncated_dists)):
+            if truncated_dists[i] > self.L:
+                end = waypoints[i]
+                break
+        if end is None or i == 0:
+            print(f"Error couldn't find valid waypoint!, {self.L}, {end}, {i}")
+            # Here assume that we are "lost"
+            # Find the nearest waypoint, set this as the current waypoint and recall this function
+            i = np.argmin(dists)
 
+            self.curr_waypoint = i
+            self.viz_point(curr_waypoints[i], 0, thick=True)
+            return curr_waypoints[i], curr_speeds[dists.argmin()]
+
+        # Interpolate
+        start = waypoints[i - 1]
+        interp = (self.L - truncated_dists[i - 1]) / (truncated_dists[i] - truncated_dists[i - 1]) # interpolation factor
+        
+        interp_waypoint = start + interp * (end - start)
+        if not np.isclose(np.linalg.norm(interp_waypoint - pose), self.L, rtol=1e-2):
+            print(f"Interpolation is not within .01 meters of L, {self.L}")
+            #raise ValueError("Interpolation is not within .01 meters of L")
+
+        self.curr_waypoint = (self.curr_waypoint + i - 1) % len(curr_waypoints)
+
+        self.viz_point(interp_waypoint, 0, thick=True)
+
+        return interp_waypoint, curr_speeds[dists.argmin()]
+    
+    def odom_callback(self, msg):
+        # Find the current waypoint to track using methods mentioned in lecture
+        pose = msg.pose.pose.position
+        self.viz_path()
+        
+        # Find waypoint and convert to car frame
+        # get a rotation matrix from the quaternion
+        # rot_mat = quaternion_matrix([pose_msg.pose.pose.orientation.x, pose_msg.pose.pose.orientation.y, pose_msg.pose.pose.orientation.z, pose_msg.pose.pose.orientation.w])
+        rot_mat = Rotation.from_quat([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]).as_matrix()
+        # homography matrix to transform waypoint to frame of car
+        H = np.eye(4)
+        H[:3, :3] = rot_mat[:3, :3]
+        H[:3, 3] = [pose.x, pose.y, pose.z]
+        H = np.linalg.inv(H)
+        waypoint, speed = self.find_waypoint(pose)
+
+        waypoint = H @ np.concatenate([waypoint, [0, 1]]) # Add a 0, 1 for the z, homography
+        waypoint = (waypoint / waypoint[-1])[:-1] # normalize and remove last element
+
+                # Calculate curvature/steering angle 2|y|/L^2
+        curvature = 2 * np.abs(waypoint[1]) / (self.L ** 2)
+
+        # Calculate and publish steering angle (clip to min and max steering angle)
+        steering_angle = np.clip(curvature * self.K, -.36, .36) * np.sign(waypoint[1])
+        print(f"Steering angle: {steering_angle}, curvature: {curvature}")
+        self.drive_pub.publish(AckermannDriveStamped(drive=AckermannDrive(steering_angle=steering_angle, speed=speed)))
 
 def main(args=None):
     rclpy.init(args=args)
-    print("WallFollow Initialized")
-    wall_follow_node = WallFollow()
-    rclpy.spin(wall_follow_node)
+    print("Overtaking Initialized")
+    overtaking_node = OverTaking()
+    rclpy.spin(overtaking_node)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    wall_follow_node.destroy_node()
+    overtaking_node.destroy_node()
     rclpy.shutdown()
 
 
